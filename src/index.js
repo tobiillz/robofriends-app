a895f8746db07ea6a418be27a5b80aed25473083
Tobiillz <ayoadeopeoluwa@yahoo.com>
2021-09-21 01:27:27 +0100

    added id-property

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import {robots} from './robots'
 ReactDOM.render(
   <React.StrictMode>
     <div>
-      <Card name={robots[0].name} email={robots[0].email}/>
-      <Card name={robots[1].name} email={robots[1].email}/>
-      <Card name={robots[2].name} email={robots[2].email}/>
+      <Card name={robots[0].name} email={robots[0].email} id={robots[0].id}/>
+      <Card name={robots[1].name} email={robots[1].email} id={robots[1].id}/>
+      <Card name={robots[2].name} email={robots[2].email} id={robots[2].id}/>
     </div>
     
   </React.StrictMode>,
