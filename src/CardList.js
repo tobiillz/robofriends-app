04882c38ab6af7a494f36fac878361fca08c1ebe
Tobiillz <ayoadeopeoluwa@yahoo.com>
2021-09-22 11:54:00 +0100

    Added cardlist component

--- src/CardList.js
+++ src/CardList.js
@@ -2,14 +2,13 @@ import react from "react";
 // import { robots } from "./robots";
 import Card from "./card";
 
-const CardList = () =>{
-
+const CardList = ({robots}) =>{
+    const cardComponent = robots.map((user)=>())  
     return(
         <>
             <Card name={robots[0].name} email={robots[0].email} id={robots[0].id}/>
             <Card name={robots[1].name} email={robots[1].email} id={robots[1].id}/>
             <Card name={robots[2].name} email={robots[2].email} id={robots[2].id}/>
-            <Card name={robots[9].name} email={robots[9].email} id={robots[9].id}/>
         </>
     )
 }
