69af0011ea8ce8589674d5ce891102970e10748c
Tobiillz <ayoadeopeoluwa@yahoo.com>
2021-09-24 09:14:10 +0100

    Started state management

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React ,  { Component }  from 'react'
 import CardList from './CardList'
 import {robots} from './robots'
 import SearchBox from './SearchBox'
-function App() {
-    return (
-        <div className="tc">
-            <h1 className="tc">Robofriends</h1>
-            <SearchBox/>
-            <CardList robots={robots} />
-        </div>
-    )
+
+
+
+class App extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange =(event)=>{
+        console.log(event.target.value)
+        const filteredRobots = this.state.robots.filter(robot => {
+                return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+            }
+        )
+    }
+
+    render(){
+        return (
+            <div className="tc">
+                <h1 className="tc">Robofriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={this.state.robots} />
+            </div>
+        )
+    }
 }
 
 export default App
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-function SearchBox() {
+const SearchBox = ({searchfield, searchChange})=> {
     return (
         <div ClassName="pa2" >
             <input 
             className ="pa3 ba b--green bg-lightest-blue"
             type="Search" 
             placeholder ="Search Robot"
+            onChange ={searchChange}
             />
         </div>
     )
