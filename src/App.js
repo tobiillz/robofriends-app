07354c75990c6b5c25142bf1ea1f982acf107fa2
Tobiillz <ayoadeopeoluwa@yahoo.com>
2021-10-04 10:50:20 +0100

    Returning an empty array of bots

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
