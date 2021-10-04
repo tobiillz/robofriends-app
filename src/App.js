de38e4c18a66b4098ddeaa6db805c491f9ad7005
Tobiillz <ayoadeopeoluwa@yahoo.com>
2021-10-04 01:13:57 +0100

    working search bar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import SearchBox from './SearchBox'
 
 
 class App extends Component {
-
     constructor(){
         super()
         this.state = {
@@ -15,22 +14,21 @@ class App extends Component {
         }
     }
 
-    onSearchChange =(event)=>{
-        console.log(event.target.value)
-        const filteredRobots = this.state.robots.filter(robot => {
-                return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-            }
-        )
+    onSearchChange =(event) =>{
+        this.setState({searchfield: event.target.value})  
+        // console.log(filteredRobots) 
     }
-
     render(){
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
         return (
             <div className="tc">
-                <h1 className="tc">Robofriends</h1>
+                <h1>Robofriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={this.state.robots} />
+                <CardList robots={filteredRobots} />
             </div>
-        )
+        );
     }
 }
 
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SearchBox = ({searchfield, searchChange})=> {
     return (
-        <div ClassName="pa2" >
+        <div className="pa2" >
             <input 
             className ="pa3 ba b--green bg-lightest-blue"
             type="Search" 
