3d0736d6a62d607b60f8a6e4d166b9626643bfc4
Tobiillz <ayoadeopeoluwa@yahoo.com>
2021-10-10 23:31:42 +0100

    Consuming the users api

--- src/App.js
+++ src/App.js
@@ -15,6 +15,13 @@ class App extends Component {
         }
     }
 
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response=> response.json())
+        .then(users => this.setState({robots: users}))
+        
+    }
+
     onSearchChange =(event) =>{
         this.setState({searchfield: event.target.value})  
         // console.log(filteredRobots) 
