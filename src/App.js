61affd8210735826aa2c6b5244bd8cdc9fa9e246
Tobiillz <ayoadeopeoluwa@yahoo.com>
2021-10-17 22:13:44 +0100

    before scroll

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
     componentDidMount(){
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response=> response.json())
-        .then(users => this.setState({robots: users}))
+        .then(users => {this.setState({robots: users})})
         
     }
 
@@ -30,13 +30,18 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-        return (
-            <div className="tc">
-                <h1 className="f2">Robofriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots} />
-            </div>
-        );
+        if (this.state.robots.length === 0){
+            return <h1> Loading </h1>
+        }else{
+            return (
+                <div className="tc">
+                    <h1 className="f2">Robofriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots} />
+                </div>
+            );
+        }
+        
     }
 }
 
